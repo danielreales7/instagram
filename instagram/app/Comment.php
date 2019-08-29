5c8106c97a533949bb123581d759abe714ae024f
Daniel Reales Harana <daniel@MacBook-Pro-de-Daniel.local>
2019-08-29 18:41:16 +0200

    Login y registros de usuarios

--- instagram/app/Comment.php
+++ instagram/app/Comment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $table = 'comments';
+
+    // Relación de Muchos a Uno
+    public function user() {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    // Relación de Muchos a Uno
+    public function image() {
+        return $this->belongsTo('App\Image', 'image_id');
+    }
 }
--- instagram/app/Image.php
+++ instagram/app/Image.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    protected $table = 'images';
+
+    // Relación One To Many / de uno a muchos
+    public function comments() {
+        return $this->hasMany('App\Comment');
+    }
+
+    // Relación One To Many
+    public function likes() {
+        return $this->hasMany('App\Like');
+    }
+
+    // Relación de Muchos a Uno
+    public function user() {
+        return $this->belongsTo('App\User', 'user_id');
+    }
 }
