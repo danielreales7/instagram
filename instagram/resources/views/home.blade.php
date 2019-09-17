5e34195306454102bb95e64a424b8aa458679e6a
daniel.reales <daniel.reales@palladiumhotelgroup.com>
2019-09-17 15:50:10 +0200

    Creacion helpers Formatear fechas

--- instagram/resources/views/home.blade.php
+++ instagram/resources/views/home.blade.php
@@ -32,6 +32,9 @@
                         <span class="nickname">
                             {{ '@'.$image->user->nick }}
                         </span>
+                        <span class="nickname date">
+                            {{ ' | '. FormatTime::LongTimeFilter($image->created_at) }}
+                        </span>
                         <p>
                             {{ $image->description }}
                         </p>
