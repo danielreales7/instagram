66920b842c8eac618c92435c57dc38e0c83c60b2
Daniel Reales Harana <daniel@MacBook-Pro-de-Daniel.local>
2019-10-15 19:01:07 +0200

    LikeController

--- instagram/routes/web.php
+++ instagram/routes/web.php
@@ -27,3 +27,5 @@ Route::get('/image/file/{filename}', 'ImageController@getImage')->name('image.fi
 Route::get('/imagen/{id}', 'ImageController@detail')->name('image.detail');
 Route::get('/comment/delete/{id}', 'CommentController@delete')->name('comment.delete');
 Route::post('/comment/save/', 'CommentController@save')->name('comment.save');
+Route::get('/like/{image_id]/', 'LikeController@like')->name('like.save');
+Route::get('/dislike/{image_id]/', 'LikeController@dislike')->name('like.delete');
